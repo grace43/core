ba46182ca7274b8d5c10163b6407c70fa9895f6d
Filip Linde <flindu@gmail.com>
2023-12-06 13:46:01 +0000

    Setup test. TODO make fixture for coordinator

--- tests/components/openAQ/__init__.py
+++ tests/components/openAQ/__init__.py
@@ -7,9 +7,12 @@ from unittest.mock import patch
 def patch_setup_entry() -> bool:
     """Patch interface."""
     return patch("homeassistant.components.openAQ.async_setup_entry", return_value=True)
+<<<<<<< HEAD
 =======
 """Tests for OpenERZ component."""
 >>>>>>> 0137d8c7a9 (create test folder)
 =======
 """Tests for openAQ component."""
 >>>>>>> 0ef05b56a4 (mock api using client (#25))
+=======
+>>>>>>> 4d38476d3a (Setup test. TODO make fixture for coordinator)
--- tests/components/openAQ/conftest.py
+++ tests/components/openAQ/conftest.py
@@ -10,11 +10,50 @@ from homeassistant.components.openAQ.coordinator import OpenAQDataCoordinator
 
 =======
 """Provide common fixtures for tests."""
+from collections.abc import Awaitable, Callable
 from unittest.mock import Mock, patch
 
 import pytest
 
+<<<<<<< HEAD
 >>>>>>> 0ef05b56a4 (mock api using client (#25))
+=======
+from homeassistant.components.openAQ.const import API_KEY_ID, DOMAIN, LOCATION_ID
+from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
+
+from tests.common import MockConfigEntry
+
+ComponentSetup = Callable[[MockConfigEntry], Awaitable[None]]
+
+
+@pytest.fixture(name="config_entry")
+def mock_config_entry() -> MockConfigEntry:
+    """Create openAQ entry in Home Assistant."""
+    return MockConfigEntry(
+        domain=DOMAIN,
+        title="openAQ",
+        data={
+            API_KEY_ID: 1234,
+            LOCATION_ID: 1234,
+        },
+    )
+
+
+@pytest.fixture(name="setup_integration")
+async def mock_setup_integration(
+    hass: HomeAssistant,
+) -> Callable[[MockConfigEntry], Awaitable[None]]:
+    """Fixture for setting up the component."""
+
+    async def func(mock_config_entry: MockConfigEntry) -> None:
+        mock_config_entry.add_to_hass(hass)
+        assert await async_setup_component(hass, DOMAIN, {})
+        await hass.async_block_till_done()
+
+    return func
+
+>>>>>>> 4d38476d3a (Setup test. TODO make fixture for coordinator)
 
 class MockAQClientSuccess:
     """Mock for AQClient that simulates a successful response."""
